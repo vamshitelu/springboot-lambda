e2b1e2912a0daeb63652e0abd1ee27fd1fef033c
vamsh <vamshikrishna.telu@gmail.com>
2025-08-13 00:09:31 +0530

    db-subnet available zones increase to 2

--- infra/main.tf
+++ infra/main.tf
@@ -14,16 +14,20 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "lambda_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}a"
+  availability_zone = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_subnet" "db_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "${var.aws_region}a"
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
 resource "aws_security_group" "lambda_sg" {
